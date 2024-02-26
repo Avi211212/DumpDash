a3e8a42f062b31a4a8859915f121c7b03431017d
Animesh Sharma <animeshsharma1996@gmail.com>
2024-02-26 19:31:53 +0000

    New Google Play Games Login/Logout Functions

--- Elephant Rush/Assets/_Scripts/Socials/GoogleInit.cs
+++ Elephant Rush/Assets/_Scripts/Socials/GoogleInit.cs
@@ -12,9 +12,75 @@ using System;
 public class GoogleInit : MonoBehaviour
 {
     [SerializeField] private Text detailsText;
-    Task task;
+    
+    private PlayGamesClientConfiguration clientConfiguration;
 
-    bool isSignedIn = false;
+    private bool IsSignedIn => PlayGamesPlatform.Instance.IsAuthenticated();
+
+    private void Start()
+    {
+        ConfigureGPGS();
+
+        if(IsSignedIn)
+        {
+            detailsText.text = Social.localUser.userName;
+        }
+        else
+        {
+            detailsText.text = "Login";
+        }
+    }
+
+    internal void ConfigureGPGS()
+    {
+        clientConfiguration = new PlayGamesClientConfiguration.Builder().EnableSavedGames().Build();
+    }
+
+    internal void SignIntoGPGS(SignInInteractivity interactivity, PlayGamesClientConfiguration configuration)
+    {
+        configuration = clientConfiguration;
+        PlayGamesPlatform.InitializeInstance(configuration);
+        PlayGamesPlatform.Activate();
+
+        PlayGamesPlatform.Instance.Authenticate(interactivity, (code) =>
+        {
+            Debug.Log("Authenticating..");
+
+            if (code == SignInStatus.Success)
+            {
+                Debug.Log("Sign in successful");
+                detailsText.text = Social.localUser.userName;
+            }
+            else
+            {
+                Debug.Log("Sign in failed" + code);
+                detailsText.text = "Failed";
+            }
+        });
+    }
+
+    public void LoginLogoutButton()
+    {
+        if (IsSignedIn)
+        {
+            SignOut();
+        }
+        else
+        {
+            SignIn();
+        }
+    }
+
+    private void SignIn()
+    {
+        SignIntoGPGS(SignInInteractivity.CanPromptAlways, clientConfiguration);
+    }
+
+    private void SignOut()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+        detailsText.text = "Login";
+    }
 
 }
 #endif
